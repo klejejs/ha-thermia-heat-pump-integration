4111c795e794973ebb03c3b6012e853179ccf2c1
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2021-12-30 20:05:40 +0200

    Added dependency update workflow

---
+++ scripts/update_ThermiaOnlineAPI_version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+manifest_file="./custom_components/thermia/manifest.json"
+
+latest_version=ThermiaOnlineAPI==$(lastversion ThermiaOnlineAPI --at pip)
+
+echo "Last version of ThermiaOnlineAPI: $latest_version"
+
+current_version=$(jq '.requirements[0]' $manifest_file | tr -d '"')
+
+echo "Current version of ThermiaOnlineAPI: $current_version"
+
+if [ "$current_version" != "$latest_version" ]; then
+  echo "Updating version to $latest_version"
+  version=$latest_version jq ".requirements = [env.version]" $manifest_file > tmp.$$.json && mv tmp.$$.json $manifest_file
+  echo "ThermiaOnlineAPI version updated to $latest_version"
+else
+  echo "Version $current_version is already up to date"
+fi
