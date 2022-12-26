d4d294f54368a481c7955c47922ca841102a5aa0
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2022-12-26 18:31:31 +0200

    Add hot water boost switch

--- custom_components/thermia/switch.py
+++ custom_components/thermia/switch.py
@@ -7,6 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .switches.hot_water_switch import ThermiaHotWaterSwitch
+from .switches.hot_water_boost_switch import ThermiaHotWaterBoostSwitch
 
 from .const import DOMAIN
 
@@ -20,11 +21,13 @@ async def async_setup_entry(
 
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
 
-    hass_thermia_hot_water_switches = [
-        ThermiaHotWaterSwitch(coordinator, idx)
-        for idx, heat_pump in enumerate(coordinator.data.heat_pumps)
-        if heat_pump.is_hot_water_switch_available
-        and heat_pump.hot_water_switch_state is not None
-    ]
+    hass_thermia_switches = []
 
-    async_add_entities(hass_thermia_hot_water_switches)
+    for idx, heat_pump in enumerate(coordinator.data.heat_pumps):
+        if heat_pump.hot_water_switch_state is not None:
+            hass_thermia_switches.append(ThermiaHotWaterSwitch(coordinator, idx))
+
+        if heat_pump.hot_water_boost_switch_state is not None:
+            hass_thermia_switches.append(ThermiaHotWaterBoostSwitch(coordinator, idx))
+
+    async_add_entities(hass_thermia_switches)
