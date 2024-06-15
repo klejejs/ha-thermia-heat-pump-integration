612af7fa58c99c98560918593bdef69f77660e86
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-06-16 00:33:46 +0300

    Improve auto API update script commit message and PR title

--- scripts/update_ThermiaOnlineAPI_version.sh
+++ scripts/update_ThermiaOnlineAPI_version.sh
@@ -15,6 +15,7 @@ if [ "$current_version" != "$latest_version" ]; then
   echo "Updating version to $latest_version"
   version=$latest_version jq ".requirements = [env.version]" $manifest_file > tmp.$$.json && mv tmp.$$.json $manifest_file
   echo "ThermiaOnlineAPI version updated to $latest_version"
+  echo "LATEST_API_VERSION=$latest_version" >> $GITHUB_ENV
 else
   echo "Version $current_version is already up to date"
 fi
