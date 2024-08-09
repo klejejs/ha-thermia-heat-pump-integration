41b13bec2b5e12b540e129807133fa89752da159
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-08-09 14:49:59 +0300

    Improve development environment, bump requirements

--- scripts/update_ThermiaOnlineAPI_version.sh
+++ scripts/update_ThermiaOnlineAPI_version.sh
@@ -2,6 +2,7 @@
 set -e
 
 manifest_file="./custom_components/thermia/manifest.json"
+requirements_file="./requirements.txt"
 
 latest_version_number=$(lastversion ThermiaOnlineAPI --at pip)
 latest_version=ThermiaOnlineAPI==$latest_version_number
@@ -15,6 +16,10 @@ echo "Current version of ThermiaOnlineAPI: $current_version"
 if [ "$current_version" != "$latest_version" ]; then
   echo "Updating version to $latest_version"
   version=$latest_version jq ".requirements = [env.version]" $manifest_file > tmp.$$.json && mv tmp.$$.json $manifest_file
+
+  # update requirements file to reflect correct version as well
+  cat $requirements_file | sed -E "s/$current_version/$latest_version/" > tmp.$$.txt && mv tmp.$$.txt $requirements_file
+
   echo "ThermiaOnlineAPI version updated to $latest_version"
   echo "LATEST_API_VERSION=$latest_version_number" >> $GITHUB_ENV
 else
